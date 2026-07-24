23df8dcb19d6703ebe58887751be3f0ba90d9a18
Sindhurajk <117331925+Sindhurajk@users.noreply.github.com>
2026-07-24 21:00:56 +0530

    Update variables.tf

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -17,6 +17,6 @@ variable "jwt_secret" {
 }
 
 variable "github_org" {
-  description = "Your GitHub username or organization that owns zen-pharma-frontend and zen-pharma-backend (e.g. john-smith)"
+  description = "sindhurajk"
   type        = string
 }
